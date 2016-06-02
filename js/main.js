05bb6e212a9388c21afa75e95ce5102f6b7eacde
Matt Ossman <foxmulder900@yahoo.com>
2016-06-01 23:42:36 -0400

    Initial Commit

---
+++ js/main.js
@@ -0,0 +1,111 @@
+(function(){
+	var stage = d3.select('#stage'),
+		bounds = stage.node().getBoundingClientRect(),
+		data = [];
+
+	var colorScale = d3.scale.linear()
+		.domain([0, 200000])
+		.range(['#969300', '#B1F800']);
+
+	var forceLayout = d3.layout.force()
+		.nodes(data)
+		.size([bounds.width, bounds.height-150])
+		.charge(-60)
+		.chargeDistance(800)
+		.on("tick", updateData);
+
+	function updateData(){
+		var userBubbles = stage.selectAll('circle')
+			.data(forceLayout.nodes(), function(d){
+				return d.username;
+			});
+
+		userBubbles
+			.attr("cx", function(d) { return d.x; })
+			.attr("cy", function(d) { return d.y; })
+			.style("fill", function(d){
+				return colorScale(new Date() - d.createdTimestamp);
+			})
+			.style("stroke", function(d){
+				return d.flaggedPosts > 10 ? 'red' : '#270043';
+			});
+
+		userBubbles.enter().append('circle')
+			.attr('r', 15)
+			.on('click', removeDatum)
+			.on('mouseover', showDetails)
+			.on('mouseout', hideDetails);
+
+		userBubbles.exit()
+			.remove();
+		
+		forceLayout.start();
+	}
+
+	function showDetails(d){
+		var tooltip = stage.append("g")
+			.classed('tooltip', true)
+			.style("pointer-events", "none")
+			.attr("transform", function() { return "translate("+d.x+","+d.y+")"; });
+
+		tooltip.append("rect")
+			.attr("width", 230)
+			.attr("height", 80)
+			.style("stroke", "#7D01D7")
+			.style("fill", "#3B0065");
+
+		tooltip.append("foreignObject")
+			.attr('x', 5)
+			.attr('y', 5)
+			.attr("width", 220)
+			.attr("height", 70)
+			.append("xhtml:body")
+			.style("background-color", "#3B0065")
+			.html(buildDetails(d));
+	}
+
+	function buildDetails(d){
+		var sessionAge = Math.ceil((new Date() - d.createdTimestamp)/60/60);
+		return "<b>"+d.username+"</b><br>" +
+			   "Session age: "+ sessionAge+" minute(s)<br>" +
+			   "Flagged posts: "+ d.flaggedPosts;
+
+	}
+
+	function hideDetails(){
+		stage.selectAll("g.tooltip").remove();
+	}
+
+	function removeDatum(d){
+		hideDetails();
+		var idx = data.indexOf(d);
+		data.splice(idx, 1);
+	}
+
+	d3.select('#login-button').on('click', function(){
+		var input = d3.select('input[name=username]'),
+			username = input.property('value');
+		if(username){
+			createUserSession();
+			updateData();
+			input.property('value','')
+		}
+	});
+
+	d3.select('#random-button').on('click', function(){
+		for(var i=0; i<10; i++){
+			createUserSession(USERNAMES[Math.floor(Math.random() * USERNAMES.length)]);
+		}
+		updateData();
+	});
+
+	function createUserSession(username){
+		data.push({
+			username: username,
+			flaggedPosts: Math.floor(Math.random()*12),
+			createdTimestamp: new Date(),
+			x: Math.random() * bounds.width,
+			y: bounds.height - Math.random()*150
+		});
+	}
+})();
